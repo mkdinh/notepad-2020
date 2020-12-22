f03eeebae57e3047ca34e81ee30f159453b9cd62
Michael Dinh <mkdinh94@gmail.com>
2020-12-21 19:34:09 -0500

    integrate with postgres

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
+    'DATABASE_URL', 'sqlite:///notepad.sqlite')
+
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
 
 
 @app.route('/')
 def index():
     return "Hello world!"
+
+
+@app.route('/api/tasks-postgres')
+def getTasksPostgres():
+    tasks = db.session.query(Task)
+    data = []
+
+    for task in tasks:
+        item = {
+            'id': task.id,
+            'description': task.description
+        }
+        data.append(item)
+
+    return jsonify(data)
 
 
 if __name__ == '__main__':
